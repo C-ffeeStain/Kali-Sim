7ffc042e7cc4df916be7a6ed97b8e9d7d36b15eb
C-ffeeStain <deaconavery2006@gmail.com>
2021-02-07 14:53:17 -0600

    Create settings file in setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,15 @@ exe_data = []
 with open("main.exe", "rb") as f:
    exe_data = f.readlines()
 os.remove("main.exe")
-os.system(f"mkdir \"{install_dir}\"")
+if not os.path.exists(install_dir):
+    os.system(f"mkdir \"{install_dir}\"")
+
 new_pos = open(f"{install_dir}KaliSim_ConsoleEditon.exe", "wb")
 new_pos.writelines(exe_data)
 new_pos.close()
+
+cfg = open(f"{install_dir}settings.ini", "w")
+cfg.writelines(["[settings]", "check_for_updates = true"])
+cfg.close()
+
 logger.updater(f"Finished installing to \"{install_dir}!\"")
