52c569256626ce7efaac2ae8a33f8982465a59b4
C-ffeeStain <deaconavery2006@gmail.com>
2021-02-08 07:21:26 -0600

    made setup.py download and build main.py

--- setup.py
+++ setup.py
@@ -1,22 +1,41 @@
 import os
 import logger
+import requests
+import shutil
 
 install_dir = f"{os.getenv('APPDATA')}\\Kali Sim - CE\\"
+url = "https://github.com/C-ffeeStain/Kali-Sim/raw/main/main.py"
 
 logger.updater("Installing...")
-os.system("python -m pip install requests")
-exe_data = []
-with open("main.exe", "rb") as f:
-   exe_data = f.readlines()
-os.remove("main.exe")
-if not os.path.exists(install_dir):
-    os.system(f"mkdir \"{install_dir}\"")
+os.system("pip install -r requirements.txt")
+request = requests.get(url)
+if request.status_code == requests.codes.ok:
+    with open("main.py", "w") as f:
+        f.write(request.text)
+os.system("pyinstaller main.py")
 
-new_pos = open(f"{install_dir}KaliSim_ConsoleEditon.exe", "wb")
+exe_data = []
+with open("dist\\main.exe", "rb") as f:
+    exe_data = f.readlines()
+if not os.path.exists(install_dir):
+    os.mkdir(install_dir)
+
+new_pos = open(install_dir + "KaliSim_ConsoleEditon.exe", "wb")
 new_pos.writelines(exe_data)
 new_pos.close()
 
-cfg = open(f"{install_dir}settings.ini", "w")
+try:
+    shutil.rmtree("build")
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
+
+try:
+    shutil.rmtree("dist")
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
+
+# Create and write to the config file 
+cfg = open(install_dir + "settings.ini", "w")
 cfg.writelines(["[settings]", "check_for_updates = true"])
 cfg.close()
 
