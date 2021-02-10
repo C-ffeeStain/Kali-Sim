33fb89b763bcebb29dc7968a84c800dfc1a64858
C-ffeeStain <deaconavery2006@gmail.com>
2021-02-10 05:37:16 -0600

    fixed logger issues

--- logger.py
+++ logger.py
@@ -1,9 +1,7 @@
 import sys
 
 def debug(message):
-    if sys.stdout.isatty():
-        print("[debug]: " + message)
+    print("[debug]: " + message)
 
 def updater(message):
-    if sys.stdout.isatty():
-        print(u"\u001b[35m [updater]: \u001b[35;1m" + message + "\u001b[0m")
+    print("[updater]: " + message)
